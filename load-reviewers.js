4bff91e4c4465859816a95b7fea65a99d3a5c568
OfficialCRUGG <ich@crugg.de>
2024-06-09 00:16:09 +0200

    ci: try to fix assign-reviewers

--- load-reviewers.js
+++ load-reviewers.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
 
-const labels = process.env.LABELS.split(" ");
+const labelsJson = process.env.LABELS;
+const labelsArray = JSON.parse(labelsJson).map((label) => label.name);
 const fileContents = fs.readFileSync(".github/reviewers.yml", "utf8");
 const reviewers = yaml.load(fileContents);
 let assignedReviewers = [];
 
-labels.forEach((label) => {
+labelsArray.forEach((label) => {
   if (reviewers[label]) {
     assignedReviewers = assignedReviewers.concat(reviewers[label]);
   }
@@ -15,5 +16,5 @@ labels.forEach((label) => {
 if (assignedReviewers.length > 0) {
   console.log("::set-output name=reviewers::" + assignedReviewers.join(","));
 } else {
-  console.log("No reviewers found for the labels " + labels.join(", "));
+  console.log("No reviewers found for the labels " + labelsArray.join(", "));
 }
